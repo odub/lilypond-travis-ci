d3f029df660ed4b9fd8db37a15a34939e046a9b0
Oscar Dub <oscardub@gmail.com>
2015-07-03 16:57:39 +0200

    Add echos to install script

--- scripts/install-travis-prereqs.sh
+++ scripts/install-travis-prereqs.sh
@@ -6,8 +6,10 @@
 
 sudo apt-get update -qq
 
+echo "Installing texlive"
 sudo apt-get install -qq texlive
 
+echo "Installing LilyPond"
 wget -q http://download.linuxaudio.org/lilypond/binaries/linux-64/lilypond-2.18.2-1.linux-64.sh
 
 sudo sh lilypond-2.18.2-1.linux-64.sh --batch
